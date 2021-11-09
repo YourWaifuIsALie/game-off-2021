46982b49d0c0b6c6ca4b3107d6cf31c74ba6cd51
YourWaifuIsALie <yourwaifuisalie@gmail.com>
2021-11-08 21:37:11 -0500

    day7
    looking at streaming assets

--- game-off-2021/Assets/Scripts/BattleTag.cs
+++ game-off-2021/Assets/Scripts/BattleTag.cs
@@ -4,26 +4,3 @@ public class BattleTag
     public string shortDescription { get; set; }
     public string longDescription { get; set; }
 }
-
-// TODO: Move this somewhere sensible
-public class EffectCondition
-{
-    // TODO: Standard way to store json/read in Unity
-    // logic: { and: { origin: [womanizer], target: [woman]}},
-    // dynamic logic = JObject.Parse("{number:1000, str:'string', array: [1,2,3,4,5,6]}");
-    // var filePath = Application.persistentDataPath + "/gamedata.json";
-    // string fileContents = File.ReadAllText(filePath);
-    // File.WriteAllText(filePath, "{\"test\": \"hi\"}");
-    // using Newtonsoft.Json;
-    // using System.IO;
-    public EffectCondition()
-    {
-
-    }
-
-    public bool evaluate(BattleActor origin, BattleActor target)
-    {
-        // TODO: Parse boolean logic
-        return false;
-    }
-}
